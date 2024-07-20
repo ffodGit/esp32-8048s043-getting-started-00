6fab9aa9d0c450c92c713126aa54606dff527753
ffodGit <ffodgit@github.com>
2024-07-20 11:20:51 +0800

    feat(0000): add platformio arduino lvgl 9.1.0 template

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include <Arduino.h>
+#include <lvgl.h>
+
+#include "lovyanGfxSetup.h"
+
+#define TFT_HOR_RES SCREEN_WIDTH
+#define TFT_VER_RES SCREEN_HEIGHT
+
+/* LVGL draws into this buffer, 1/10 screen size usually works well. The size is in bytes. */
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+LGFX tft;
+
+#if LV_USE_LOG != 0
+void my_print(lv_log_level_t level, const char *buf)
+{
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display. */
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+  uint32_t w = lv_area_get_width(area);
+  uint32_t h = lv_area_get_height(area);
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.writePixels((lgfx::rgb565_t *)px_map, w * h);
+  tft.endWrite();
+
+  /* Call it to tell LVGL you are ready. */
+  lv_disp_flush_ready(disp);
+}
+
+/* Read the touchpad. */
+void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
+{
+  uint16_t touchX, touchY;
+  bool touched = tft.getTouch(&touchX, &touchY);
+
+  if (!touched)
+  {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_PRESSED;
+    data->point.x = touchX;
+    data->point.y = touchY;
+#if 1
+    Serial.printf("x: %03d, y: %03d\n", data->point.x, data->point.y);
+#endif
+  }
+}
+
+void setup()
+{
+  String LVGL_Arduino = "Hello Arduino ";
+  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+  Serial.begin(115200);
+  Serial.println(LVGL_Arduino);
+
+  tft.begin();
+  tft.setRotation(0);
+  tft.setBrightness(32);
+
+  lv_init();
+
+  /* Set a tick source so that LVGL will know how much time elapsed. */
+  lv_tick_set_cb((lv_tick_get_cb_t)millis);
+
+  /* Register print function for debugging. */
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print);
+#endif
+
+  /* Create a display. */
+  lv_display_t *disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+  lv_display_set_flush_cb(disp, my_disp_flush);
+  lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+  /* Initialize the (dummy) input device driver. */
+  lv_indev_t *indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+  lv_indev_set_read_cb(indev, my_touchpad_read);
+
+  Serial.println("Setup done");
+
+#if 1
+  /* Create a simple label. */
+  lv_obj_t *label = lv_label_create( lv_scr_act() );
+  lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+  lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+#endif
+
+}
+
+void loop()
+{
+  lv_task_handler(); /* Let LVGL do its work. */
+
+  // Do something useful here.
+}
